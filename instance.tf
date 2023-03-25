189ede0f4a1aa45000f7030d4fe5669f544ac413
SkyrunnerOO7 <punitpawar08@gmail.com>
2023-03-25 12:04:23 +0530

    vpc

---
+++ instance.tf
@@ -0,0 +1,44 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dovekey"
+  public_key = file(var.PUB_KEY)
+}
+
+resource "aws_instance" "dove-web" {
+  ami                    = var.AMIS[var.REGION]
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.dove-pub-1.id
+  key_name               = aws_key_pair.dove-key.key_name
+  vpc_security_group_ids = [aws_security_group.dove_stack_sg.id]
+  tags = {
+    Name    = "Dove-Instance"
+    Project = "my-dove"
+  }
+
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod u+x /tmp/web.sh",
+      "sudo /tmp/web.sh"
+    ]
+  }
+
+  connection {
+    user        = var.USER
+    private_key = file("dovekey")
+    host        = self.public_ip
+  }
+}
+
+
+
+output "PublicIP" {
+  value = aws_instance.dove-web.public_ip
+}
+
+output "PrivateIP" {
+  value = aws_instance.dove-web.private_ip
+}
---
+++ vars.tf
@@ -0,0 +1,39 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "ZONE3" {
+  default = "us-east-1c"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-02f3f602d23f1659d"
+    us-east-2 = "ami-02f97949d306b597a"
+  }
+}
+
+variable "USER" {
+  default = "ec2-user"
+}
+
+variable "PUB_KEY" {
+  default = "dovekey.pub"
+}
+
+variable "PRIV_KEY" {
+  default = "dovekey"
+}
+
+variable "MYIP" {
+  default = "0.0.0.0/0"
+}
